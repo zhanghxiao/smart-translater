e78a2056f8b01b6e37f029ed19491f63b530907a
habacat <pb_hbc@163.com>
2024-07-29 16:05:16 +0800

    fix

--- functions/api.js
+++ functions/api.js
@@ -1,11 +1,25 @@
 export async function onRequest(context) {
-	const { request } = context;
-	// 临时响应所有请求，用于测试函数是否活跃
-	return new Response('Hello from API function!', {
-		status: 200,
-		headers: {
-			'Content-Type': 'text/plain',
-			"Access-Control-Allow-Origin": "*"
-		}
-	});
+	const { request, env } = context;
+	const url = new URL(request.url);
+	if (request.method === "OPTIONS") {
+		return new Response(null, {
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+				"Access-Control-Allow-Headers": "Content-Type",
+			}
+		});
+	}
+	// 根据请求路径修改请求目的地
+	if (url.pathname.startsWith("/api")) {
+		const newUrl = new URL(url);
+		newUrl.pathname = url.pathname.replace('/api', '');
+		newResponse = env.API_WORKER.fetch(new Request(newUrl.toString(), request));
+		newResponse.headers.set('Access-Control-Allow-Origin', '*');
+		newResponse.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT');
+		newResponse.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+		return newResponse;
+	} else {
+		return new Response('Not Found', { status: 404 });
+	}
 }
