d4c2f9b37bcd3ab2977efaead08813959586a01c
zhanghxiao <137479165+zhanghxiao@users.noreply.github.com>
2024-07-30 22:06:14 +0800

    try

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,5 @@
 module.exports = {
-  devServer: {
-    port: 7000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': '/api'  // 这里不需要重写路径
-        }
-      }
-    }
-  },
-  publicPath: process.env.NODE_ENV === 'production' ? './' : './' ,
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   chainWebpack: config => {
     config.plugin('html')
       .tap(args => {
